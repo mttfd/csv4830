99a1520d22ef6d8ea2b6953bdb19ed18cd6de416
Mingqin Tan <mttfd@mail.missouri.edu>
2015-09-17 10:37:35 -0500

    done exploration1

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+var readcsv = require('./lib/readcsv'),
+    writecsv = require('./lib/writecsv');
 
+var csvArr = readcsv.getCSVObject("/Users/tanmingqin/Desktop/asdasd.txt", function(csv) {
+    console.log(csv)
+});
 
+writecsv.writeToFile("./test.txt", [['a', 'a', 'b'], ['b', 'c', 'd']]);
